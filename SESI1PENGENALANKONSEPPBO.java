c8cf5f23acdb479d41aa3f88dbe89d1547519341
silviapriliyanti <114372347+silviapriliyanti@users.noreply.github.com>
2022-09-26 05:37:21 +0300

    Add files via upload

---
+++ SESI1PENGENALANKONSEPPBO.java
@@ -0,0 +1,10 @@
+package sesi1pengenalankonseppbo;
+
+public class SESI1PENGENALANKONSEPPBO {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+    
+}
